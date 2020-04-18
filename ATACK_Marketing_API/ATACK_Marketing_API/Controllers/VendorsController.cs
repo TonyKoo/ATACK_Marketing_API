18485f4ab5ab7d32d81708ed13904e3220c5878d
TonyKoo <46759104+TonyKoo@users.noreply.github.com>
2020-04-18 15:21:17 -0700

    Add Events Basic Functionality (Find/List All)

---
+++ ATACK_Marketing_API/ATACK_Marketing_API/Controllers/VendorsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ATACK_Marketing_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ATACK_Marketing_API.Controllers
+{
+    [Route("api/{eventId}/[controller]")]
+    [ApiController]
+    public class VendorsController : ControllerBase {
+        private readonly MarketingDbContext _context;
+
+        public VendorsController(MarketingDbContext context) {
+            _context = context;
+        }
+
+
+    }
+}
