32a94ca0eee11ab94a9ee37aea44711168d62db8
TonyKoo <46759104+TonyKoo@users.noreply.github.com>
2020-04-18 22:37:37 -0700

    Add Event Join/Leave/List

---
+++ ATACK_Marketing_API/ATACK_Marketing_API/Repositories/EventGuestRepository.cs
@@ -0,0 +1,63 @@
+﻿using ATACK_Marketing_API.Data;
+using ATACK_Marketing_API.Models;
+using ATACK_Marketing_API.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATACK_Marketing_API.Repositories {
+    public class EventGuestRepository {
+        private readonly MarketingDbContext _context;
+
+        public EventGuestRepository(MarketingDbContext context) {
+            _context = context;
+        }
+
+        public UserEventListViewModel GetUsersEvents(User theUser) {
+            ICollection<UserEventListEventDetailViewModel> eventsJoined = _context.EventGuests.Where(eg => eg.User == theUser).Select(eg => new UserEventListEventDetailViewModel {
+                EventId = eg.Event.EventId,
+                EventName = eg.Event.EventName,
+                EventStartDateTime = eg.Event.EventDateTime
+            }).ToList();
+
+            return new UserEventListViewModel {
+                UserEmail = theUser.Email,
+                EventsJoined = eventsJoined
+            };
+        }
+
+        public bool JoinEvent(User theUser, Event theEvent) {
+            bool isSuccessful = false;
+
+            try {
+                EventGuest newSubscription = new EventGuest {
+                    User = theUser,
+                    Event = theEvent
+                };
+
+                _context.EventGuests.Add(newSubscription);
+                _context.SaveChanges();
+                isSuccessful = true;
+            } catch (Exception) {
+
+            }
+
+            return isSuccessful;
+        }
+
+        public bool LeaveEvent(EventGuest theEventJoined) {
+            bool isSuccessful = false;
+
+            try {
+                _context.EventGuests.Remove(theEventJoined);
+                _context.SaveChanges();
+                isSuccessful = true;
+            } catch (Exception) {
+
+            }
+
+            return isSuccessful;
+        }
+    }
+}
---
+++ ATACK_Marketing_API/ATACK_Marketing_API/ViewModels/EventGuestViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATACK_Marketing_API.ViewModels {
+
+    public class EventGuestViewModel {
+        public int EventId { get; set; }
+        public String UserEmail { get; set; }
+        public String EventName { get; set; }
+        public bool Joined { get; set; }
+    }
+
+    public class UserEventListViewModel {
+        public String UserEmail { get; set; }
+        public ICollection<UserEventListEventDetailViewModel> EventsJoined { get; set; }
+    }
+    
+    public class UserEventListEventDetailViewModel {
+        public int EventId { get; set; }
+        public String EventName { get; set; }
+        public DateTime EventStartDateTime { get; set; }
+    }
+}
