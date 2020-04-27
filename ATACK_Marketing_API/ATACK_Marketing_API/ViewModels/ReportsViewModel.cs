9df365e3251e315bc8cac7479e37103a49401315
TonyKoo <46759104+TonyKoo@users.noreply.github.com>
2020-04-27 00:35:47 -0700

    Reporting

---
+++ ATACK_Marketing_API/ATACK_Marketing_API/ViewModels/ReportsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ATACK_Marketing_API.ViewModels {
+    public class VendorSubscriberReportViewModel {
+        public int EventId { get; set; }
+        public String EventName { get; set; }
+        public DateTime EventStartDateTime { get; set; }
+        public int EventVendorId { get; set; }
+        public String VendorName { get; set; }
+        public ICollection<VendorSubscriberDetailViewModel> Subscribers { get; set; }
+    }
+
+    public class VendorSubscriberDetailViewModel {
+        public String UserEmail { get; set; }
+    }
+}
